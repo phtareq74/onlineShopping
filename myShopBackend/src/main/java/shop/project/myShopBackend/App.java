169ba3bc539a8348139e39ca8f1617cb271cc32e
phtareq <tareq.hammid@viqtordavis.com>
2019-09-28 09:50:33 +0200

    first commit- first setup

---
+++ myShopBackend/src/main/java/shop/project/myShopBackend/App.java
@@ -0,0 +1,13 @@
+package shop.project.myShopBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
